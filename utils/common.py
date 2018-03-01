07d0dbe7cd5c479172657798523b55c3d4470680
you <1570819807@qq.com>
2018-03-01 23:12:24 +0800

    添加还原装饰器函数名操作

--- utils/common.py
+++ utils/common.py
@@ -1,8 +1,10 @@
 # coding:utf-8
+import functools
 
 
 def require_login(fun):
     """验证登录装饰器"""
+    @functools.wraps(fun)
     def wapper(requesthandler, *args, **kwargs):
         if requesthandler.check_user_login():
             fun(requesthandler, *args, **kwargs)
