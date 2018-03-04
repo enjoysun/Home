14064385bc7c9e8de29d46501b4abe42bbbedd18
you <1570819807@qq.com>
2018-03-04 22:37:03 +0800

    添加映射

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 import os
 from handler.RequestHandler import *
-from handler import VerifyCode, RegisterAndLogin
+from handler import VerifyCode, RegisterAndLogin, UserCenter
 # from tornado.web import StaticFileHandler
 from handler.BaseRequestHandler import StaticFileHandler
 urls = [
@@ -11,5 +11,8 @@ urls = [
     (r'/api/register', RegisterAndLogin.RegisterHandler),
     (r'/api/login', RegisterAndLogin.LoginHandler),
     (r'/api/check_login', RegisterAndLogin.CheckUserLoginHandler),
+    (r'/api/profile/avatar', UserCenter.UserImageUpload),
+    (r'/api/profile', UserCenter.UserInfoHandler),
+    (r'/api/profile/name', UserCenter.UserNameUploadHandler),
     (r'/(.*?)$', StaticFileHandler, dict(path=os.path.join(os.path.dirname(__file__), "html"), default_filename="index.html")),
 ]
